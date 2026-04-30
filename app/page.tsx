b5798f75407ec70d9a9ab26ab7ed0535aee2d41f
rishabh <1244rishabh@gmail.com>
2026-04-30 12:20:01 +0530

    done

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
 import Navbar from "./components/Navbar/Navbar";
+import Hero from "./components/landing_page_components/Hero/Hero";
+
 export default function Home() {
-  return (
-    <>
-      <Navbar />
-
-
-   </>
-  );
+    return (
+        <>
+            <Navbar />
+            <Hero />
+        </>
+    );
 }
