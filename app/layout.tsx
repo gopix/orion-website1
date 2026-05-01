8ac919631b573350a411bfbf2bf5cde38de96828
rishabh <1244rishabh@gmail.com>
2026-05-01 09:15:39 +0530

    responsive

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,8 +4,8 @@ import type { Metadata } from "next";
 export const metadata: Metadata = {
   title: "Orion Systems",
   description: "Intelligent Digital Systems for Publishing",
+      viewport: "width=device-width, initial-scale=1",
 };
-
 export default function RootLayout({
   children,
 }: {
